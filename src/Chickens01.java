d0aa5bb594752bbb21628df65ceef627e5d10e67
Pittwo <malcolm.molloy@stu.ocsb.ca>
2022-09-21 14:38:48 -0400

    the math is wrong
    i hate my math and brain

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -1,7 +1,19 @@
 public class Chickens01 {
     public static void main(String[] args) {
         //Put your code here
-        
-        System.out.println(totalEggs);
+        int totalEggs, eggsPerChicken=5, ChickenCount=3;
+        //Monday calculations
+        System.out.println("on monday the amount of chickens the farmer had was:");
+        System.out.println(ChickenCount+1);
+        //Tuesday calculations
+        System.out.println("on tuesday morning the amount of eggs the farmer had was:");
+        System.out.println(eggsPerChicken= eggsPerChicken*(ChickenCount*2)+(1*3) );
+        //Wednsday
+        System.out.println("on wednesday morning a wild best came and ate half of the farmers chickens leaving him with left with only");
+        System.out.println((ChickenCount+1)/2);
+        System.out.println("by noon the number of eggs the farmer has is:");
+        System.out.println(eggsPerChicken=eggsPerChicken+(eggsPerChicken+ChickenCount-1));
+        System.out.println("the number of eggs the farmer has collected is:");
+        System.out.println(totalEggs=eggsPerChicken*(ChickenCount-1));
     }   
 }
