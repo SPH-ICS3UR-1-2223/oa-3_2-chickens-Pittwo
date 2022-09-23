d5c78f8e4992ae1d61c4f9d508e630020d6a394c
Pittwo <malcolm.molloy@stu.ocsb.ca>
2022-09-23 14:37:51 -0400

    Update Chickens02.java

--- src/Chickens02.java
+++ src/Chickens02.java
@@ -1,9 +1,27 @@
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
-
+    	double dailyAverage=0;
+    	double eggCount=0;
+    	double monthlyAverage=0;
+    	double monthlyProfit=0;
+    	//Monday
+    	eggCount+=100; //collect 100 eggs
+    	//Tuesday
+    	eggCount+=121; //collect 121 eggs
+    	//Wednesday
+    	eggCount+=117; //collect 117 eggs
+    	
+    	//calculate daily average
+    	dailyAverage = eggCount/3;
         System.out.println("Daily Average:   " +dailyAverage);
+        
+        //calculate the Monthly average
+        monthlyAverage 
         System.out.println("Monthly Average: " +monthlyAverage);
+        
+        //calculate monthly profit
+        
         System.out.println("Monthly Profit:  $" +monthlyProfit);
     }
     
